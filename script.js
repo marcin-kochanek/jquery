3860c9ea8b99fa129abf117262b6d619f0b459ee
Marcin Kochanek <md.kochanek@gmail.com>
2017-03-16 13:39:05 +0100

    Exercise 9.2

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function(){
+	console.log('DOM załadowany - można się bawić');
+});
+
+$( 'span:odd' ).css( 'color', 'red' );
+
+var paragraphs = $( 'p' );
+
+paragraphs.each(function(index, element){
+
+	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+	$(element).append(button);
+
+});
+
+$( 'button' ).click(function(){
+
+	alert($(this).attr( 'data-tmp' ));
+});
