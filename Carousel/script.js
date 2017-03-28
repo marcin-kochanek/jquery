ae6070d6c3cd7e46b0f18daf8d0ba2d572c80aeb
Marcin Kochanek <md.kochanek@gmail.com>
2017-03-28 22:33:48 +0200

    The final version

--- Carousel/script.js
+++ Carousel/script.js
@@ -1,25 +1,26 @@
 $(function() {
-	var carouselList = $( '#carousel ul'),
+	var carouselList = $('#carousel ul'),
 		interval;
 	
 	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+		var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
 
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	}
 
 	function moveLastSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+		var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
 
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft: -500});	
 	}
 
-	function nextSlide() {	
-		carouselList.animate({marginLeft: -500}, 500, moveFirstSlide);
+	function nextSlide() {
+		moveFirstSlide();
+		carouselList.animate({marginLeft: -500}, 500);
 	}
 
 	function prevSlide() {
